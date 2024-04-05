589c94278117baf9a1d600b7c8df5c0e8a47b6ba
nikulin alexandr <testerunfake0002@gmail.com>
2024-04-06 02:54:16 +0300

    6

--- RMP4-/app/src/main/java/com/example/rmp4/MainActivity.java
+++ RMP4-/app/src/main/java/com/example/rmp4/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rmp4;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Intent intent = new Intent(this, OverlayService.class);
+        if (Settings.canDrawOverlays(getApplicationContext())) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent);
+            }
+            else {
+                startService(intent);
+            }
+        }
     }
 }
--- RMP4-/app/src/main/java/com/example/rmp4/fragments/StartFragment.java
+++ RMP4-/app/src/main/java/com/example/rmp4/fragments/StartFragment.java
@@ -1,19 +1,41 @@
 package com.example.rmp4.fragments;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.example.rmp4.R;
 
 public class StartFragment extends Fragment {
-
+    private final String ID = "channelID";
+    private final int notificationID = 9;
     public StartFragment() {
         super(R.layout.fragment_start);
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(ID, "Channel 9", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("It's channel 9");
+            NotificationManager notificationManager = (NotificationManager)requireContext()
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_start, container, false);
@@ -37,10 +59,33 @@ public class StartFragment extends Fragment {
                 Navigation.findNavController(v).navigate(R.id.from_start_to_recycler, bundle);
             }
         });
+        view.findViewById(R.id.notification).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(requireContext(), ID)
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentTitle("Test")
+                        .setContentText("Your soul is mine!")
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireContext());
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+                    return;
+                notificationManager.notify(notificationID, builder.build());
+            }
+        });
 
         return view;
     }
 }
+
+
+
+
+
+
+
 
 
 
